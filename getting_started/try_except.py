8c7ea201498826f1ef79c688c1c87c4cb5a9c23b
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2021-01-29 18:52:50 -0500

    notes

--- getting_started/try_except.py
+++ getting_started/try_except.py
@@ -1,18 +1,19 @@
-# Try works, skips Except
-a_str = '123'
-try:
-  int_str = int(a_str)
-except:
-  int_str = -1
-print('Second', int_str)
-
 # Try fails, jumps to Except
 # wherever the code fails in the try block
 a_str = 'hello'
 try:
   print('before fail')
-  int_str = int(a_str)
-  print('after fail')
+  int_str = int(a_str)   # code fails here
+  print('after fail')    # this line never prints
 except:
-  int_str = -1
-print('First', int_str)notes
+  int_str = -1           # jumps to except
+print('First', int_str)  # int_str = -1
+
+
+# Try works, skips Except
+a_str = '123'
+try:
+  int_str = int(a_str)   # '123' can be converted from str to int
+except:                  # skips Except
+  int_str = -1 
+print('Second', int_str) # int_str = 123
